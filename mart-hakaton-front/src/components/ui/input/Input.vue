21b9201236a192195106febbfba8e34c6d847634
Igor Kretov <knigor1337@gmail.com>
2024-03-15 14:53:03 +0300

    added components input

---
+++ mart-hakaton-front/src/components/ui/input/Input.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { useVModel } from '@vueuse/core'
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+  defaultValue: { type: [String, Number], required: false },
+  modelValue: { type: [String, Number], required: false },
+  class: { type: null, required: false }
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+  passive: true,
+  defaultValue: props.defaultValue
+})
+</script>
+
+<template>
+  <input
+    v-model="modelValue"
+    :class="
+      cn(
+        'flex h-10 rounded-md border border-input px-3 py-2 text-sm outline-none w-80 border-purple-500 bg-purple-200 focus:border-transparent focus:ring-purple-500 focus:ring-2',
+        props.class
+      )
+    "
+  />
+</template>
--- mart-hakaton-front/src/views/AuthPage.vue
+++ mart-hakaton-front/src/views/AuthPage.vue
@@ -2,9 +2,15 @@
   <div class="px-10 py-5">
     <p>Libraria</p>
     <p>Храните знания и учитесь!</p>
+    <Input class="mt-5" placeholder="ФИО" />
+    <Input class="mt-5" placeholder="Логин пользователя" />
+    <Input class="mt-5" placeholder="Пароль" />
+    <Input class="mt-5" placeholder="Дата рождения" />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { Input } from '@/components/ui/input'
+</script>
 
 <style lang="scss" scoped></style>
